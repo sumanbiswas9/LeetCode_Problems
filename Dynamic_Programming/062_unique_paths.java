1757cdf2be9cbd893ea77a0db9c4b87db0346c5d
Suman Biswas <128128172+sumanbiswas9@users.noreply.github.com>
2026-04-24 19:54:30 +0530

    Add: 62. Unique Paths [Medium]

---
+++ Dynamic_Programming/062_unique_paths.java
@@ -0,0 +1,12 @@
+// ============================================================
+// Problem    : 62. Unique Paths
+// Difficulty : Medium
+// URL        : https://leetcode.com/problems/unique-paths/
+// Language   : Java
+// Date       : 2026-04-24
+// ============================================================
+class Solution {
+    public int uniquePaths(int m, int n) {
+        
+    }
+}
