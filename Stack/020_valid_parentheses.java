b266931bdd48eb202f13221c00a7746b9e848f58
Suman Biswas <128128172+sumanbiswas9@users.noreply.github.com>
2026-04-24 23:10:00 +0530

    Add: 20. Valid Parentheses [Easy]

---
+++ Stack/020_valid_parentheses.java
@@ -0,0 +1,12 @@
+// ============================================================
+// Problem    : 20. Valid Parentheses
+// Difficulty : Easy
+// URL        : https://leetcode.com/problems/valid-parentheses/
+// Language   : Java
+// Date       : 2026-04-24
+// ============================================================
+class Solution {
+    public boolean isValid(String s) {
+        
+    }
+}
