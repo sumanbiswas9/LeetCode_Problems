d0ce3a4fde2d324ec4412921cad26f7b8b988f7a
Suman Biswas <128128172+sumanbiswas9@users.noreply.github.com>
2026-04-28 12:42:23 +0530

    Add: 2769. Find the Maximum Achievable Number [Easy]

---
+++ Math/2769_find_the_maximum_achievable_number.java
@@ -0,0 +1,12 @@
+// ============================================================
+// Problem    : 2769. Find the Maximum Achievable Number
+// Difficulty : Easy
+// URL        : https://leetcode.com/problems/find-the-maximum-achievable-number/
+// Language   : Java
+// Date       : 2026-04-28
+// ============================================================
+class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+        return num+(2*t);
+    }
+}
