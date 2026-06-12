7069063d6a6d2d3fc34f5fac345ba87e3dd8c657
Suman Biswas <128128172+sumanbiswas9@users.noreply.github.com>
2026-06-12 10:08:49 +0530

    Add: 3559. Number of Ways to Assign Edge Weights II [Hard]

---
+++ Daily_Questions/3559_number_of_ways_to_assign_edge_weights_ii.java
@@ -0,0 +1,112 @@
+// ============================================================
+// Problem    : 3559. Number of Ways to Assign Edge Weights II
+// Difficulty : Hard
+// URL        : https://leetcode.com/problems/number-of-ways-to-assign-edge-weights-ii/
+// Language   : Java
+// Date       : 2026-06-12
+// ============================================================
+class Solution {
+
+    static final long MOD = 1_000_000_007L;
+
+    int LOG;
+    int[][] up;
+    int[] depth;
+    long[] pow2;
+    List<Integer>[] graph;
+
+    public int[] assignEdgeWeights(int[][] edges, int[][] queries) {
+        int n = edges.length + 1;
+
+        graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] e : edges) {
+            int u = e[0];
+            int v = e[1];
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        LOG = 1;
+        while ((1 << LOG) <= n) LOG++;
+
+        up = new int[n + 1][LOG];
+        depth = new int[n + 1];
+
+        dfs(1, 0);
+
+        for (int j = 1; j < LOG; j++) {
+            for (int i = 1; i <= n; i++) {
+                up[i][j] = up[up[i][j - 1]][j - 1];
+            }
+        }
+
+        pow2 = new long[n + 1];
+        pow2[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            pow2[i] = (pow2[i - 1] * 2) % MOD;
+        }
+
+        int m = queries.length;
+        int[] ans = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            int u = queries[i][0];
+            int v = queries[i][1];
+
+            int lca = lca(u, v);
+            int len = depth[u] + depth[v] - 2 * depth[lca];
+
+            if (len == 0) {
+                ans[i] = 0;
+            } else {
+                ans[i] = (int) pow2[len - 1];
+            }
+        }
+
+        return ans;
+    }
+
+    private void dfs(int node, int parent) {
+        up[node][0] = parent;
+
+        for (int nxt : graph[node]) {
+            if (nxt == parent) continue;
+
+            depth[nxt] = depth[node] + 1;
+            dfs(nxt, node);
+        }
+    }
+
+    private int lca(int a, int b) {
+
+        if (depth[a] < depth[b]) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        int diff = depth[a] - depth[b];
+
+        for (int j = 0; j < LOG; j++) {
+            if (((diff >> j) & 1) == 1) {
+                a = up[a][j];
+            }
+        }
+
+        if (a == b) return a;
+
+        for (int j = LOG - 1; j >= 0; j--) {
+            if (up[a][j] != up[b][j]) {
+                a = up[a][j];
+                b = up[b][j];
+            }
+        }
+
+        return up[a][0];
+    }
+}
