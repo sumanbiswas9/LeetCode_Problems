aaea949f97cb8fa9a084e66853e8868228a7abf1
Suman Biswas <128128172+sumanbiswas9@users.noreply.github.com>
2026-07-09 09:40:13 +0530

    Add: 3532. Path Existence Queries in a Graph I [Medium]

---
+++ Daily_Questions/3532_path_existence_queries_in_a_graph_i.java
@@ -0,0 +1,30 @@
+// ============================================================
+// Problem    : 3532. Path Existence Queries in a Graph I
+// Difficulty : Medium
+// URL        : https://leetcode.com/problems/path-existence-queries-in-a-graph-i/
+// Language   : Java
+// Date       : 2026-07-09
+// ============================================================
+class Solution {
+    public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+
+        int[] comp = new int[n];
+        int id = 0;
+        comp[0] = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] - nums[i - 1] > maxDiff) {
+                id++;
+            }
+            comp[i] = id;
+        }
+
+        boolean[] ans = new boolean[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            ans[i] = comp[queries[i][0]] == comp[queries[i][1]];
+        }
+
+        return ans;
+    }
+}
