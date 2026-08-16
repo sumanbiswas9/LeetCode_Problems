8d951b2f5b1cc25bd5a042e57f0e00be79a724dd
Suman Biswas <128128172+sumanbiswas9@users.noreply.github.com>
2026-08-16 12:01:18 +0530

    Add: 2029. Stone Game IX [Medium]

---
+++ Daily_Questions/2029_stone_game_ix.java
@@ -0,0 +1,29 @@
+// ============================================================
+// Problem    : 2029. Stone Game IX
+// Difficulty : Medium
+// URL        : https://leetcode.com/problems/stone-game-ix/
+// Language   : Java
+// Date       : 2026-08-16
+// ============================================================
+class Solution {
+
+    public boolean stoneGameIX(int[] stones) {
+        int cnt0 = 0,
+            cnt1 = 0,
+            cnt2 = 0;
+        for (int val : stones) {
+            int type = val % 3;
+            if (type == 0) {
+                ++cnt0;
+            } else if (type == 1) {
+                ++cnt1;
+            } else {
+                ++cnt2;
+            }
+        }
+        if (cnt0 % 2 == 0) {
+            return cnt1 >= 1 && cnt2 >= 1;
+        }
+        return cnt1 - cnt2 > 2 || cnt2 - cnt1 > 2;
+    }
+}
